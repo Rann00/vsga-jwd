35700df8bc46d6ba799bf97b287b48e20f2a56d0
Rann00 <113426709+Rann00@users.noreply.github.com>
2024-05-21 16:25:12 +0700

    Add files via upload

---
+++ tugas_satu.php
@@ -0,0 +1,7 @@
+<?php
+echo "Nama : Puji Barata <br>".
+ "Asal Kampus : Universitas Muhammadiyah Kotabumi <br>".
+ "Jurusan : Sistem dan Teknologi Informasi <br>".
+ "Kota : Kotabumi, Lampung Utara, Lampung <br>".
+ "PHP : Pernah <br>".
+ "HTML : Pernah";
